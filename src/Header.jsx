e82e37437c2fb9323d61cc61c2fc0e786e257571
eemarishkin <ee.marishkin@yandex.ru>
2024-12-05 00:35:01 +0400

    header-2

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 
 export function Header(){
     
+    const [tabA,setTabA] = useState(false)
     const [tab,setTab] = useState(false)
     const [time,setTime] = useState(new Date())
     setInterval(()=>setTime(new Date()),1000)
@@ -22,6 +23,15 @@ export function Header(){
           {tab && (
             <div className="modal">
             <img src="./public/война.jpg" alt="война" />
+            <a className="variorA" onClick={()=>setTabA(true)} href="#">Выбрать войну</a>
+              {tabA && (
+                <div className="modalInModal">
+                <a href="#">Великая Отечественная Война</a>
+                <a href="#">Гражданская Война</a>
+                <a href="#">Война в Украине</a>
+                <button className="button-modalA" onClick={()=>setTabA(false)}>Назад</button>
+               </div>
+              )}
             <button className="button" onClick={()=>setTab(false)}>Закрыть</button>
          </div>
           )}
