721d9ab48860349843217ae0f0e841a4915efabd
emarishkin <ee.marishkin@yandex.ru>
2024-12-07 17:27:43 +0400

    quiz-start

--- src/MainComp.jsx
+++ src/MainComp.jsx
@@ -1,7 +1,19 @@
 import {vars} from './data'
 import { MainVarInfo } from './mainComponents/MainVarInfo'
+import { Game } from './mainComponents/Game'
+import { questions } from './questions'
+import { Result } from './mainComponents/Result'
+import { useState } from 'react'
 
 export function MainComp(){
+    const [step,setStep] = useState(0)
+    const question=questions[step]
+    
+    const clickVAriant= (index)=>{
+        console.log(step,index)
+        setStep(step+1)
+    }
+
     return (
         <section className='top'>
             <div className='main'>
@@ -13,6 +25,10 @@ export function MainComp(){
                      {/* <MainVarInfo {...vars[0]} /> 
                      <MainVarInfo {...vars[1]} /> 
                      <MainVarInfo {...vars[2]} />  */}
+                </div>
+                <div className='quiz'>
+                    <Game question={question} clickVAriant={clickVAriant}/>
+                    <Result/>
                 </div>
             </div>
         </section>
---
+++ src/mainComponents/Result.jsx
@@ -0,0 +1,9 @@
+export function Result(){
+    return(
+        <div className="result">
+            <img src="" alt="" />
+            <h2>Вы отгадали 5 вопросов</h2>
+            <button>Попробовать снова</button>
+        </div>
+    )
+}
